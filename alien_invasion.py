e9ee33fdba0a28b1ea8afb64dccde52af8e5af32
Rahul Tevatia <rtevatia2@gmail.com>
2024-03-10 09:21:58 -0400

    edited the file

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 
 def run_game():
     pygame.init()
-    screen = pygame.display.setmode((1200, 800))
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
     # Set the background color
     bg_color = (230,230,230)
