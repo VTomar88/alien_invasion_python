22a3442e26ca4c82806e850960aa18a29584737e
Rahul Tevatia <rtevatia2@gmail.com>
2024-03-10 20:08:16 -0400

    Added the ship to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,27 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
+    # Initialize pygame, settings, and screen objects
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
-    # Set the background color
-    bg_color = (230,230,230)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height
+    ))
+    # Make ship
+    ship = Ship(screen)
+    print(ship.rect)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         # Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         pygame.display.flip()
 
