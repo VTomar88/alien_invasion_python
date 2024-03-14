04c2e886a5fd015b1bf1f7034f18bc101772c738
Varsha Tomar <varshatomar88@gmail.com>
2024-03-14 17:53:10 -0400

    Added changes to move shift to right

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,7 +16,8 @@ def run_game():
     ship = Ship(screen)
 
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
