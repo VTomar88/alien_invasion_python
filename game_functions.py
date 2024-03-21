322eefcc5837346b5ce66cfede4fe802990d9d0c
Rahul Tevatia <rtevatia2@gmail.com>
2024-03-20 22:29:47 -0400

    Fixed the alien ship

--- game_functions.py
+++ game_functions.py
@@ -35,7 +35,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship, bullets, alien):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update the images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
