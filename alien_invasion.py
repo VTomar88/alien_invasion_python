d0cbd3cd2ce8a388b7ec4717f3abfa1fcfbc7140
Varsha Tomar <varshatomar88@gmail.com>
2024-03-23 08:53:47 -0400

    added a fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ def run_game():
     bullets = Group()
     aliens = Group()
     # Create a fleet of aliens
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
